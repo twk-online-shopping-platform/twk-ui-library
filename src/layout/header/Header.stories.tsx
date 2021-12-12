6122b42ea1a0afd60f11d49c152736a0a8895a4a
handakina <46838034+handakina@users.noreply.github.com>
2021-12-12 03:23:40 -0800

    added search text change handler

--- src/layout/header/Header.stories.tsx
+++ src/layout/header/Header.stories.tsx
@@ -2,10 +2,21 @@ import React, { ReactElement } from 'react';
 import { storiesOf, addDecorator, ComponentMeta } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
 import Header from './Header';
+import { MenuItemProps } from './MenuUtil';
 
-const authSearchOptions = [
-  { label: 'The Godfather', id: 1 },
-  { label: 'Pulp Fiction', id: 2 },
+const subMenueTestFun = ()=>{}
+const menue2ClickFun = ()=>{
+  console.log('clicked');
+}
+const submenu: MenuItemProps[] = [
+  { menuDetail: { id: 1, title: 'sub-menu 1', handler: subMenueTestFun } },
+  { menuDetail: { id: 2, title: 'sub-menu 2', handler: menue2ClickFun } },
+  { menuDetail: { id: 3, title: 'sub-menu 3', handler: subMenueTestFun } },
+
+]
+const menues: MenuItemProps[] = [
+  { menuDetail: { id: 1, title: 'menu 1', handler: subMenueTestFun } },
+  { menuDetail: { id: 2, title: 'menu 2', handler: subMenueTestFun, menuItems: submenu } },
 ];
 const searchFieldChangeHanlder = (value: string) => {
   console.log('changed', value);
@@ -13,4 +24,5 @@ const searchFieldChangeHanlder = (value: string) => {
 
 storiesOf('MaterialUI', module)
   .addDecorator(muiTheme())
-  .add('MainHeader', () => (<Header searchFieldHandler={searchFieldChangeHanlder} />));
+  .add('MainHeader', () => (<Header searchFieldHandler={searchFieldChangeHanlder} />))
+  .add('MainHeaderWithMenu', () => (<Header menus={menues} searchFieldHandler={searchFieldChangeHanlder} />));

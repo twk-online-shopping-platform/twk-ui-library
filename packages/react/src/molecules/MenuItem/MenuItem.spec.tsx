3694511cb3b91d916385a0d235f02a08dc130459
handakina <46838034+handakina@users.noreply.github.com>
2022-10-16 13:30:51 -0700

    menuitem test passed

--- packages/react/src/molecules/MenuItem/MenuItem.spec.tsx
+++ packages/react/src/molecules/MenuItem/MenuItem.spec.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import MenuItem from "./MenuItem";
 import { menuItemTestId, subMenuItemTestId } from "./MenuItemConstants";
-import { MenuItemSize, MenuItemType } from "./Type";
+import { MenuItemSize, MenuItemType, SubMenuPosition } from "./Type";
 
 describe("Test MenuItem Component", () => {
   it("should display MenuItem Component", async () => {
@@ -22,7 +22,21 @@ describe("Test MenuItem Component", () => {
   });
   it("should accept click handler", async () => {
     const menuItemProps: MenuItemType = {
-      label: "",
+      label: "Test",
+      submenu: {
+        position: SubMenuPosition.LEFT,
+        menu: {
+          menuItems: [
+            {
+              label: "Social Media",
+              leftIcon: "fa-solid fa-house-user",
+              submenu: {
+                position: SubMenuPosition.LEFT,
+              },
+            },
+          ],
+        },
+      },
     };
     render(<MenuItem {...menuItemProps} />);
     const muenuItem = await screen.getByTestId(subMenuItemTestId);

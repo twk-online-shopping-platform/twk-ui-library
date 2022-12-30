0df8afa522677d2294c88bf0aaf94dd9556fe5e7
handakina <46838034+handakina@users.noreply.github.com>
2022-12-30 00:03:16 -0800

    style: direct style import
    import css file directly to component
    
    BREAKING CHANGE: style display for ui component could be affected

--- packages/react/src/atoms/Field/TextField.stories.tsx
+++ packages/react/src/atoms/Field/TextField.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import TextField from "./TextField";
 import { Meta, Story } from "@storybook/react";
-import "@fortawesome/fontawesome-free/css/all.css";
 
 export default {
   title: "Atoms/TextField",
